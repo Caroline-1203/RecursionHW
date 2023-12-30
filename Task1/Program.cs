871b664aaf17e1221a21bc6a5b3a5f87fcd3da79
KarolinaTad <k.t.1993@bk.ru>
2023-12-30 22:37:09 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+int M = 3;
+int N = 9;
+
+PrintListNumber(M,N);
+
+void PrintListNumber(int start, int end)
+{
+    if(start <= end)
+    {
+        Console.WriteLine(start);
+        PrintListNumber(start + 1, end);
+    }
+}
+                    
